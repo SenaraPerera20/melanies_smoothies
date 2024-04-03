ddca7e65cc94f981235cc6acba17b9372e898eff
SenaraPerera20 <165760788+SenaraPerera20@users.noreply.github.com>
2024-04-03 12:56:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -37,15 +38,17 @@ ingredients_list = st.multiselect(
 
 # If ingredients _list is not null: then do everything below this line that is intended.
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
+    
+        
         #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
         #st.subheader(fruit_chosen + 'Nutrition Information')
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         
@@ -64,11 +67,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect ()
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
     
- # This is  the secssion to display fruityvice nutrition information.
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
+
 
 
 
